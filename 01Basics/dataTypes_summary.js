c6fcc92f4086848c1edc095d0d884e2a906f7989
Rohan Singh <70824393+RohanSingh1311@users.noreply.github.com>
2024-01-03 09:54:41 +0000

    Stack&Heap

--- 01Basics/dataTypes_summary.js
+++ 01Basics/dataTypes_summary.js
@@ -1,6 +1,6 @@
 // #primitive
 
-// 7 types: Striing, NUmber, Boolean, null. undefined, Symbol
+// 7 types: Striing, Number, Boolean, null. undefined, Symbol
 
 
 const score =100
@@ -36,3 +36,40 @@ const myFunction = function(){
 console.log(typeof bigNUmber);
 console.log(typeof outsideTemp);
 console.log(typeof animals);
+
+
+//*************************************************************************
+
+//stack(used in primitive) -> stack workson the basiosof copy
+//Heap used in non primitive ->heap uses reference
+
+let name1="Rohan"
+let name2 = name1
+
+name2= "deadlock"
+
+console.log();
+console.log(name1);
+console.log(name2);
+console.log();
+
+console.log("Before Update");
+let userOne ={
+    email: ":xyz@gmail.com",
+    contactNO: 94298122
+}
+
+let userTwo = userOne
+//this will store in Heap and both user 1 and user two points to same data 
+//so if value of userTwo changes it will affect userOne too
+
+console.log(userOne)
+console.log(userTwo);
+console.log();
+
+userTwo.email="pqr@gmsil.com"
+
+console.log("After Update");
+console.log(userOne)
+console.log(userTwo);
+console.log();
