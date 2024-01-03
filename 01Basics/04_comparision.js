f5b2462e9cf167c4c07df510a4890650e07db2de
Rohan Singh <70824393+RohanSingh1311@users.noreply.github.com>
2024-01-03 07:18:12 +0000

    comparision

---
+++ 01Basics/04_comparision.js
@@ -0,0 +1,30 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log();
+
+console.log("2">1)
+console.log("02">1)
+console.log(1<"2")
+console.log()
+
+console.log(null>0)                        //false
+console.log(null<0)                        //false
+console.log(null==0)                       //false
+console.log(null>=0) //null gets converted //true
+console.log()
+
+console.log(undefined>0)   //false
+console.log(undefined<0)   //false
+console.log(undefined==0)  //false
+console.log(undefined>=0)  //false
+console.log()
+
+// strict check ===
+
+console.log("2"==="2");
+console.log("20"===20);
+console.log();
