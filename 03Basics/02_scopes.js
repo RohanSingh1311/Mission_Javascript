c18ee6462e9b6e89b87256398e6cef3df3287160
Rohan Singh <70824393+RohanSingh1311@users.noreply.github.com>
2024-01-05 08:28:40 +0000

    scope in depth

--- 03Basics/02_scopes.js
+++ 03Basics/02_scopes.js
@@ -26,3 +26,52 @@ if(true){
     console.log('inner x = ', x);
 }
 console.log('outer x = ', x);
+console.log();
+// ********************************************************************************
+
+function one(){
+    const username = "Rohan"
+    function two() {
+        const website = "github"
+        console.log(username);
+    }
+    //console.log(website); //it cannot be executed as it is out of its scope
+    two()
+}
+
+one()
+console.log();
+
+if (true) {
+    const username ="RohanSingh131"
+    if (username === "RohanSingh131") {
+        const website =" Github"
+        console.log(username + website);
+    }
+    // console.log(website); //website is used outside its scope
+}
+// console.log(username);  //username is used outside its scope
+console.log();
+
+
+
+//in js if we define and declare a function directly then we can call the function before declaration
+console.log(addone(3))
+function addone(num) {
+    return num+1
+}
+console.log(addone(4))
+
+
+
+//in js variable are two powerfull they can store values, objects, functions too
+//in this type of declaration we cannot call the function before declaration
+
+// console.log(addTwo(1))
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(4))
+
+//concept of hoisting
